5f66320b11145d48b697825edf5ae137eff02a50
sreghenzi <samuele.reghenzi@eng.it>
2018-05-31 09:33:14 +0200

    coverage

--- src/fnts/curry.spec.ts
+++ src/fnts/curry.spec.ts
@@ -31,4 +31,41 @@ describe('Add function', () => {
 
   });
 
+  it('should support upt to 4 paramters', () => {
+   
+    function add(x: number, y: number,z:number,k:number): number {
+        return x + y+ z;
+    }
+    const addc = curry(add);
+    const addOne = addc(1);
+    const addOneAndTwo = addc(1,2);
+    const addTillThree = addc(1,2,3);
+    const addAndFour = addc(1,2,3,4);
+    expect(addOne(2,3,4)).to.equal(add(1,2,3,4));
+    expect(addOneAndTwo(3,4)).to.equal(add(1,2,3,4));
+    expect(addTillThree(4)).to.equal(add(1,2,3,4));
+
+    
+  });
+
+  it('should support upt down to 0 paramters', () => {
+   
+    function zero(): number {
+       return 0;
+    }
+
+    function fiver(x: number, y: number,z:number,k:number, n:number): number {
+      return x + y+ z;
+  }
+
+    const zeroc = curry(zero);
+    expect(zero()).to.equal(zeroc(1));
+
+    // const fiverc = curry(fiver); <-- do not compile
+
+
+    
+  });
+
+
 });
