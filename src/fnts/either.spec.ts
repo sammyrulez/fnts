637a561939c933de757548525f93804a40c0b4f9
sreghenzi <samuele.reghenzi@eng.it>
2018-06-01 11:07:51 +0200

    test names

--- src/fnts/either.spec.ts
+++ src/fnts/either.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import 'mocha';
 import { Either} from './either'
 
-describe('Add function', () => {
+describe('An Either ', () => {
 
     it('should map right values', () => {
      
